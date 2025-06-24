823b1ffa83d26985ce6ebe7da41a7d03baa62d01
Oleg <oleg@gmail.com>
2025-06-23 20:40:17 -0500

    Deleted String IO_ERROR from FileManager

--- src/Model/FileManager.java
+++ src/Model/FileManager.java
@@ -8,8 +8,6 @@ import java.nio.file.StandardOpenOption;
 
 public class FileManager {
 
-    private final String IO_ERROR = "Error reading, writing, or creating file";
-
     public void readAndProcessFile(Modes mode, String sourceFilePath, String outputFilePath, Cipher cipher) throws IOException {
         String line;
 
